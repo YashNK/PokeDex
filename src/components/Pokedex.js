70ffd6b816d19074d4f7a36d77938d5d33d130b8
yash k <yashkamnani0@gmail.com>
2024-08-01 18:34:12 +0530

    Image updates and Error validatons

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,6 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import './Pokedex.css';
+
+const BATCH_SIZE = 100;
+const RETRY_LIMIT = 1;
 
 function Pokedex() {
   const [pokemonData, setPokemonData] = useState([]);
@@ -12,29 +15,54 @@ function Pokedex() {
   const [popUpName, setPopUpName] = useState('');
   const [popUpImage, setPopUpImage] = useState('');
   const [popUpAbilities, setPopUpAbilities] = useState([]);
+  const [popUpTypes, setPopUpTypes] = useState([]);
+  const [popUpCry, setPopUpCry] = useState('');
+  const [sortOrder, setSortOrder] = useState('asc');
+  const pokemonContainerRef = useRef(null);
+  const popUpRef = useRef(null); // Create a ref for the pop-up
+
+  const fetchPokemonDetails = async (url, retries = RETRY_LIMIT) => {
+    try {
+      const result = await axios.get(url);
+      return {
+        name: result.data.name,
+        image: result.data.sprites.other['official-artwork']?.front_default || result.data.sprites.front_default,
+        gif: result.data.sprites.other.showdown.front_default || result.data.sprites.front_default,
+        abilities: result.data.abilities.map(ability => ability.ability.name),
+        types: result.data.types.map(type => type.type.name),
+        cry: result.data.cries.latest
+      };
+    } catch (error) {
+      if (retries > 0) {
+        return fetchPokemonDetails(url, retries - 1);
+      } else {
+        return null;
+      }
+    }
+  };
+
+  const fetchBatchOfPokemons = async (pokemonList) => {
+    const pokemonDetailsPromises = pokemonList.map(pokemon => fetchPokemonDetails(pokemon.url));
+    return await Promise.all(pokemonDetailsPromises);
+  };
 
   useEffect(() => {
     const fetchAllPokemonData = async () => {
       try {
-        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=3500');
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=3000');
         const pokemonList = response.data.results;
 
-        const pokemonDetailsPromises = pokemonList.map(pokemon =>
-          axios.get(pokemon.url)
-        );
-        const pokemonDetailsResponses = await Promise.all(pokemonDetailsPromises);
+        const batchedPokemonData = [];
+        for (let i = 0; i < pokemonList.length; i += BATCH_SIZE) {
+          const batch = pokemonList.slice(i, i + BATCH_SIZE);
+          const batchData = await fetchBatchOfPokemons(batch);
+          batchedPokemonData.push(...batchData.filter(pokemon => pokemon && pokemon.image));
+        }
 
-        const allPokemonData = pokemonDetailsResponses.map(({ data }) => ({
-            name: data.name,
-            image: data.sprites.other['official-artwork']?.front_default || data.sprites.front_default,
-            abilities: data.abilities.map(ability => ability.ability.name)
-          })).filter(pokemon => pokemon.image);
-
-        setPokemonData(allPokemonData);
-        setFilteredPokemonData(allPokemonData);
-
-      } catch (err) {
-        setError(err.message);
+        setPokemonData(batchedPokemonData);
+        setFilteredPokemonData(batchedPokemonData);
+      } catch {
+        setError('An error occurred while fetching Pokémon data.');
       } finally {
         setLoading(false);
       }
@@ -48,8 +76,29 @@ function Pokedex() {
     const filteredData = pokemonData.filter(pokemon =>
       pokemon.name.toLowerCase().includes(lowercasedSearchTerm)
     );
-    setFilteredPokemonData(filteredData);
-  }, [searchTerm, pokemonData]);
+    const sortedData = filteredData.sort((a, b) => {
+      if (sortOrder === 'asc') {
+        return a.name.localeCompare(b.name);
+      } else {
+        return b.name.localeCompare(a.name);
+      }
+    });
+    setFilteredPokemonData(sortedData);
+  }, [searchTerm, pokemonData, sortOrder]);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (popUpRef.current && !popUpRef.current.contains(event.target)) {
+        handleClosePopUp();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
@@ -58,17 +107,25 @@ function Pokedex() {
   const handleKeyDown = (e) => {
     if (e.key === 'Enter') {
       setSearchTerm(inputValue);
+      if (pokemonContainerRef.current) {
+        pokemonContainerRef.current.scrollTop = 0;
+      }
     }
   };
 
   const handleBtnClick = () => {
-      setSearchTerm(inputValue);
+    setSearchTerm(inputValue);
+    if (pokemonContainerRef.current) {
+      pokemonContainerRef.current.scrollTop = 0;
+    }
   };
 
-  const handlePokecard = (name, image, abilities) => {
+  const handlePokecard = (name, image, abilities, types, cry) => {
     setPopUpName(name);
     setPopUpImage(image);
     setPopUpAbilities(abilities);
+    setPopUpTypes(types);
+    setPopUpCry(cry);
     let card = document.getElementById('pokecard');
     card.style.opacity = '100%';
     card.style.zIndex = 0;
@@ -80,48 +137,79 @@ function Pokedex() {
     card.style.zIndex = -1;
   };
 
+  const toggleSortOrder = () => {
+    setSortOrder(prevOrder => prevOrder === 'asc' ? 'desc' : 'asc');
+  };
+
   return (
     <div className="main-container">
-        <div>
-            <h1 className='title'>Poké</h1><h1 className='title-yellow'>Dex</h1>
-        </div>
-        <div className='search-container'>
-            <input
-                type='text'
-                placeholder='Search Pokémon'
-                value={inputValue}
-                onChange={handleInputChange}
-                onKeyDown={handleKeyDown}
-                className='search-bar'
-                />
-            <button className='search-btn' onClick={handleBtnClick}>Search</button>
-        </div>
-        <div className='pokemon-container'>
-        {loading && <p>Loading...</p>}
-            {filteredPokemonData.map(({ name, image, abilities }) => (
-            <div
-                onClick={() => handlePokecard(name, image, abilities)}
-                className='pokemon'
-                key={name}
-            >
-                <p>{name}</p>
-                <img className='pokemon-image' src={image} alt={name} />
-            </div>
-            ))}
-        </div>
-        <div id='pokecard' className='pokecard'>
-            <div>
 
-            <h1 className='pop-up-name'>{popUpName}</h1>
-            <img className='pop-up-img' src={popUpImage} alt={popUpName} />
-            <div className='pop-up-abilities'>
-                {popUpAbilities.map((ability, index) => (
-                    <h1 className='ability' key={index}>{ability}</h1>
-                ))}
+      <div>
+        <h1 className='title'>Poké</h1><h1 className='title-yellow'>Dex</h1>
+      </div>
+
+      <div className='search-container'>
+        <input
+          type='text'
+          placeholder='Search Pokémon'
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+          className='search-bar'
+        />
+        <button className='search-btn' onClick={handleBtnClick}>Search</button>
+      </div>
+
+      <div className='sort-container'>
+        <button className='sort-btn' onClick={toggleSortOrder}>
+          Sort by Name ({sortOrder === 'asc' ? 'Ascending' : 'Descending'})
+        </button>
+      </div>
+
+      <div className='pokemon-container' ref={pokemonContainerRef}>
+        {loading && <p>Loading...</p>}
+        {error && <p>Error: {error}</p>}
+        {filteredPokemonData.map(({ name, image, abilities, types, gif, cry }) => (
+          <div
+            onClick={() => handlePokecard(name, image, abilities, types, cry)}
+            className='pokemon'
+            key={name}
+          >
+            <p>{name}</p>
+            <img className='pokemon-image' src={gif} alt={name} />
+            <div></div>
+          </div>
+        ))}
+      </div>
+
+      <div id='pokecard' className='pokecard' ref={popUpRef}>
+        <div className='pokecard-content'>
+          <h1 className='pop-up-name'>{popUpName}</h1>
+          <img className='pop-up-img' src={popUpImage} alt={popUpName} />
+          <div className='pop-up-abilities'>
+            {popUpAbilities.map((ability, index) => (
+              <h1 className='ability' key={index}>{ability}</h1>
+            ))}
+          </div>
+
+          <div className='pop-up-types'>
+            {popUpTypes.map((type, index) => (
+              <h1 className='type' key={index}>{type}</h1>
+            ))}
+          </div>
+
+          {popUpCry && (
+            <div>
+              <audio id='pokemon-cry' src={popUpCry} />
+              <button className='play-cry-btn' onClick={() => document.getElementById('pokemon-cry').play()}>
+                Hear Pokémon Cry
+              </button>
             </div>
-            </div>
-            <button className='close-btn' onClick={handleClosePopUp}>Close</button>
+          )}
         </div>
+        <button className='close-btn' onClick={handleClosePopUp}>Close</button>
+      </div>
+
     </div>
   );
 }
